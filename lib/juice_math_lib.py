001b06bb66ba7b293c192e89ba6549bb66917b98
kasaba <kasaba@pparc.gp.tohoku.ac.jp>
2023-10-11 20:19:15 +0900

    SID-20 QL

--- lib/juice_math_lib.py
+++ lib/juice_math_lib.py
@@ -1,3 +1,6 @@
+#---------------------------------------------------------------------
+#--- Y Kasaba   2023/10/11 -------------------------------------------
+#---------------------------------------------------------------------
 import numpy as np
 # import statistics
 
@@ -13,14 +16,14 @@ def _fft_freq(n_samp, freq_array, frequency, dt):
 
 #---------------------------------------------------------------------
 # FFT power
-def _fft_power(n_samp, E_i, E_q, E_power, df, hz_mode, ave_mode):
+def _fft_power(n_samp, E_i, E_q, E_power, df, unit_mode, ave_mode):
     # Hunning
     window = np.hanning( n_samp ) 
     acf = 1/(sum(window)/n_samp)
     s = np.fft.fft((E_i - E_q * 1j) * window)
     power = np.power(np.abs(s) / n_samp, 2.0) * acf * acf
 
-    if hz_mode > 0:
+    if unit_mode > 0:
         power = power / (df * 1000)
 
     if (ave_mode == 0):
@@ -39,10 +42,10 @@ def _fft_power(n_samp, E_i, E_q, E_power, df, hz_mode, ave_mode):
 
 #---------------------------------------------------------------------
 # mean power
-def _mean_power(E_i_array, E_q_array, E_power, df, hz_mode):
+def _mean_power(E_i_array, E_q_array, E_power, df, unit_mode):
     power = np.mean(E_i_array**2 + E_q_array**2, axis=1)
 
-    if hz_mode > 0:
+    if unit_mode > 0:
         power = power / (df * 1000)
 
     E_power.extend( power )
@@ -56,3 +59,14 @@ def clean_rfi(power, kernel_size=5):
     clean_power = medfilt(power, kernel_size)
     # clean_power = minfilt(power, kernel_size)
     return clean_power
+
+
+#---------------------------------------------------------------------
+#--- HID-3 RAW data --------------------------------------------------
+#---------------------------------------------------------------------
+def hf_proc_get_stokes(p1, p2, re, im):
+    I = p1 + p2;
+    Q = p1 - p2;
+    U = re * 2.0;
+    V = im * 2.0;
+    return I, Q, U, V 
--- lib/juice_sid20_lib.py
+++ lib/juice_sid20_lib.py
@@ -1,56 +1,135 @@
-# import glob
-# import spacepy.pycdf
-import numpy as np
-
 class struct:
     pass
 
-"""
-import numpy as np
-import statistics
-
-import juice_cdf_lib as juice_cdf
-import juice_math_lib as juice_math
-"""
-
 #---------------------------------------------------------------------
-#--- HID3 ------------------------------------------------------------
+#--- SID20 ------------------------------------------------------------
 #---------------------------------------------------------------------
 def juice_getdata_hf_sid20(cdf):
 
     data = struct()
 
+    # Data
     data.EuEu = cdf['EuEu'][...]
     data.EvEv = cdf['EvEv'][...]
     data.EwEw = cdf['EwEw'][...]
+    data.EuEv_re = cdf['EuEv_re'][...]
+    data.EuEv_im = cdf['EuEv_im'][...]
+    data.EvEw_re = cdf['EvEw_re'][...]
+    data.EvEw_im = cdf['EvEw_im'][...]
+    data.EwEu_re = cdf['EwEu_re'][...]
+    data.EwEu_im = cdf['EwEu_im'][...]
+
+    data.EuiEui = cdf['EuiEui'][...]
+    data.EuqEuq = cdf['EuqEuq'][...]
+    data.EviEvi = cdf['EviEvi'][...]
+    data.EvqEvq = cdf['EvqEvq'][...]
+    data.EwiEwi = cdf['EwiEwi'][...]
+    data.EwqEwq = cdf['EwqEwq'][...]
+
+    data.EuiEvi = cdf['EuiEvi'][...]
+    data.EviEwi = cdf['EviEwi'][...]
+    data.EwiEui = cdf['EwiEui'][...]
+    data.EuqEvq = cdf['EuiEvq'][...]
+    data.EvqEwq = cdf['EviEwq'][...]
+    data.EwqEuq = cdf['EwiEuq'][...]
+
+    data.EuiEuq = cdf['EuiEuq'][...]
+    data.EviEvq = cdf['EviEvq'][...]
+    data.EwiEwq = cdf['EwiEwq'][...]
+
     data.frequency = cdf['frequency'][...]
     data.freq_step = cdf['freq_step'][...]
     data.freq_width = cdf['freq_width'][...]
 
     data.epoch = cdf['Epoch'][...]
     data.scet = cdf['SCET'][...]
-    data.N_samp = cdf['N_samp'][...]
-    data.N_step = cdf['N_step'][...]
-    data.decimation = cdf['decimation'][...]
-    data.B0_step = cdf['B0_step'][...]
 
-    # Data mode
+    data.EuiEvq = cdf['EuiEvq'][...]
+    data.EuqEvi = cdf['EuqEvi'][...]
+    data.EviEwq = cdf['EviEwq'][...]
+    data.EvqEwi = cdf['EvqEwi'][...]
+    data.EwiEuq = cdf['EwiEuq'][...]
+    data.EwqEui = cdf['EwqEui'][...]
+
+
+    # AUX
     data.U_selected = cdf['U_selected'][...]
     data.V_selected = cdf['V_selected'][...]
     data.W_selected = cdf['W_selected'][...]
-    print("U:", data.U_selected)
-    print("V:", data.V_selected)
-    print("W:", data.W_selected)
+    data.N_component = cdf['N_component'][...]      # [b2:U  b1:V  b0:W]
+    data.complex = cdf['complex'][...]
+    #
+    data.cal_signal = cdf['cal_signal'][...]
+    data.sweep_table = cdf['sweep_table'][...]      # (fixed: not defined in V.2)
+    data.onboard_cal = cdf['onboard_cal'][...]      # (not used)
+    data.BG_subtract = cdf['BG_subtract'][...]
+    data.BG_select = cdf['BG_select'][...]
+    data.FFT_window = cdf['FFT_window'][...]
+    data.RFI_rejection = cdf['RFI_rejection'][...]
+    data.Pol_sep_thres = cdf['Pol_sep_thres'][...]
+    data.Pol_sep_SW = cdf['Pol_sep_SW'][...]
+    data.overflow_U = cdf['overflow_U'][...]        # (fixed: not defined in V.2)
+    data.overflow_V = cdf['overflow_V'][...]        # (fixed: not defined in V.2)
+    data.overflow_W = cdf['overflow_W'][...]        # (fixed: not defined in V.2)
+    data.proc_param0 = cdf['proc_param0'][...]
+    data.proc_param1 = cdf['proc_param1'][...]
+    data.proc_param2 = cdf['proc_param2'][...]
+    data.proc_param3 = cdf['proc_param3'][...]
+    data.BG_downlink = cdf['BG_downlink'][...]
 
-    # CUT -- Ver.1
-    n_num = data.B0_step[0]
-    if n_num == 255:
-        data.EuEu = data.EuEu[:, 0:n_num] 
-        data.EvEv = data.EvEv[:, 0:n_num] 
-        data.EwEw = data.EwEw[:, 0:n_num] 
-        data.frequency = data.frequency[:, 0:n_num] 
-        data.freq_step = data.freq_step[:, 0:n_num] 
-        data.freq_width = data.freq_width[:, 0:n_num]     
+    data.N_block = cdf['N_block'][...]
+    data.T_RWI_U = cdf['T_RWI_U'][...]
+    data.T_RWI_W = cdf['T_RWI_W'][...]
+    data.T_HF_FPGA = cdf['T_HF_FPGA'][...]
 
-    print("data.EuEu", data.EuEu)
+    # Header
+    data.N_samp = cdf['N_samp'][...]
+    data.N_step = cdf['N_step'][...]
+    data.decimation = cdf['decimation'][...]
+    data.pol = cdf['pol'][...]
+    data.B0_startf = cdf['B0_startf'][...]
+    data.B0_stopf = cdf['B0_stopf'][...]
+    data.B0_step = cdf['B0_step'][...]
+    data.B0_repeat = cdf['B0_repeat'][...]
+    data.B0_subdiv = cdf['B0_subdiv'][...]
+    data.B1_startf = cdf['B1_startf'][...]
+    data.B1_stopf = cdf['B1_stopf'][...]
+    data.B1_step = cdf['B1_step'][...]
+    data.B1_repeat = cdf['B1_repeat'][...]
+    data.B1_subdiv = cdf['B1_subdiv'][...]
+    data.B2_startf = cdf['B2_startf'][...]
+    data.B2_stopf = cdf['B2_stopf'][...]
+    data.B2_step = cdf['B2_step'][...]
+    data.B2_repeat = cdf['B2_repeat'][...]
+    data.B2_subdiv = cdf['B2_subdiv'][...]
+    data.B3_startf = cdf['B3_startf'][...]
+    data.B3_stopf = cdf['B3_stopf'][...]
+    data.B3_step = cdf['B3_step'][...]
+    data.B3_repeat = cdf['B3_repeat'][...]
+    data.B3_subdiv = cdf['B3_subdiv'][...]
+    data.B4_startf = cdf['B4_startf'][...]
+    data.B4_stopf = cdf['B4_stopf'][...]
+    data.B4_step = cdf['B4_step'][...]
+    data.B4_repeat = cdf['B4_repeat'][...]
+    data.B4_subdiv = cdf['B4_subdiv'][...]
+
+    """
+    # CUT
+    n_num = data.B0_step[0] * data.B0_subdiv[0]
+    if n_num < data.EuEu.shape[1]:
+        print("Cut: ", data.EuEu.shape[1], " ->", n_num)
+        data.EuEu = data.EuEu[:, 0:n_num]
+        data.EvEv = data.EvEv[:, 0:n_num]
+        data.EwEw = data.EwEw[:, 0:n_num]
+        data.EuEv_re = data.EuEv_re[:, 0:n_num]
+        data.EuEv_im = data.EuEv_im[:, 0:n_num]
+        data.EvEw_re = data.EvEw_re[:, 0:n_num]
+        data.EvEw_im = data.EvEw_im[:, 0:n_num]
+        data.EwEu_re = data.EwEu_re[:, 0:n_num]
+        data.EwEu_im = data.EwEu_im[:, 0:n_num]
+        data.frequency = data.frequency[:, 0:n_num]
+        data.freq_step = data.freq_step[:, 0:n_num]
+        data.freq_width = data.freq_width[:, 0:n_num]
+    """
+
     return data
