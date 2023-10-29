24c51bc453362d8d76660d8f2fbc868502e8ca64
kasaba <kasaba@pparc.gp.tohoku.ac.jp>
2023-10-29 09:40:45 +0900

    conversion factors

--- lib/juice_cdf_lib.py
+++ lib/juice_cdf_lib.py
@@ -1,4 +1,4 @@
-# JUICE RPWI HF CDF -- 2023/10/16
+# JUICE RPWI HF CDF -- 2023/10/29
 
 import glob
 import spacepy.pycdf
@@ -7,6 +7,8 @@ import numpy as np
 class struct:
     pass
 
+#---------------------------------------------------------------------
+#--- Read CDF --------------------------------------------------------------
 #---------------------------------------------------------------------
 def juice_read_cdfs(date_str, label, ver_str="01", base_dir="/db/JUICE/juice/datasets/"):
 
@@ -27,6 +29,7 @@ def juice_read_cdfs(date_str, label, ver_str="01", base_dir="/db/JUICE/juice/dat
 
     return ret, err
 
+
 #---------------------------------------------------------------------
 #--- QL --------------------------------------------------------------
 #---------------------------------------------------------------------
@@ -36,6 +39,7 @@ def _sample_rate(decimation):
     if decimation >= 0 and decimation <= 3:
         ret = (296e+3)/(2**decimation)
     return ret
+
 
 #---------------------------------------------------------------------
 # Frequency: linear [kHz]
@@ -54,9 +58,51 @@ def _get_frequencies(n_freq, samp, sample_rate):
     f_width = np.float32(
         (i_freq * 0. + sample_rate * 0.7566) / 1000.)
     f_width = np.repeat(f_width, samp)
+
     return freq, f_step, f_width
 
+
 #---------------------------------------------------------------------
+#--- CAL --------------------------------------------------------------
+#---------------------------------------------------------------------
+# *** Conversion factor
+#   unit_mode       0: sum    1: /Hz
+#   cal_mode        0: raw    1: dBm＠ADC  2: V@HF   3:V2@HF   4:V2@RWI
+#   cal             0: background     1: CAL
+def cal_factors(unit_mode, cal_mode, cal, p_raw_max, p_raw_min):
+    cf = 0.0                                # Conversion Factor: RAW
+
+    if cal_mode == 1:
+        cf = -104.1                         # dBm @ ADC 
+    elif cal_mode == 2:
+        cf = -104.1 - 10.00 - 15.0          # V(amplitude) @ HF -- in EM2-1: HF-gain +15dB, ADC: 2Vpp  ==> EM2-3 & later: same [-6dB + 6dB]
+    elif cal_mode == 3:
+        cf = -104.1 - 13.01 - 15.0          # V^2 @ HF (EM2-0 case)
+    elif cal_mode == 4:
+        cf = -104.1 - 13.01 - 15.0 - 5.0    # V^2 @ RWIin -- temporary
+
+    # ******************************************************
+    # [EM2-0]
+    # "1-bit" = -104.1 dBm = -114.1 dB V  = 1.97E-6 V    ==> "20-bit": 2.06 Vpp
+    # "HF input"  +15dB(AMP) -3dB(50-ohm) = "+12dB"      ==> "1-bit": 5E-7 V,  Full: 0.5 Vpp
+    # ******************************************************
+    # [EM2-3]
+    # "1-bit" = -110.1 dBm = -110.1 dB V  = 0.99E-7 V "  ==> "20-bit": 1.03 Vpp
+    # "HF input"  +9dB(AMP)  -3dB(50-ohm受け) = "+6dB"    ==> "1-bit": 5E-7 V,  Full: 0.5 Vpp
+    # ******************************************************
+
+    # *** Max / Min in plots ***
+    p_max = p_raw_max + cf/10
+    p_min = p_raw_min + cf/10
+
+    # *** Unit mode: Bandwidth is needed.
+    if unit_mode == 1:
+        p_max = p_max - 4.5
+        p_min = p_min - 4.5
+
+    return cf, p_max, p_min
+
+
 # power label
 def power_label(cal_mode, unit_mode):
     if unit_mode == 0:
