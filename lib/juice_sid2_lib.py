f9fd8ff551e56b7404689634d6b71e9b5b462686
kasaba <kasaba@pparc.gp.tohoku.ac.jp>
2023-12-11 11:19:01 +0900

    SID-2: High-f-res packet checks

--- lib/juice_sid2_lib.py
+++ lib/juice_sid2_lib.py
@@ -1,5 +1,5 @@
 """
-    JUICE RPWI HF SID2 (RAW): L1a QL -- 2023/12/2
+    JUICE RPWI HF SID2 (RAW): L1a QL -- 2023/12/11
 """
 import numpy as np
 import juice_cdf_lib as juice_cdf
@@ -13,7 +13,7 @@ class struct:
 # ---------------------------------------------------------------------
 # --- SID2 ------------------------------------------------------------
 # ---------------------------------------------------------------------
-def juice_getdata_hf_sid2(cdf, cf):
+def hf_sid2_read(cdf, cf):
     """
     input:  CDF, cf:conversion factor
     return: data
@@ -108,6 +108,103 @@ def juice_getdata_hf_sid2(cdf, cf):
         print("-16 shift in 202305 data")
     # ### SPECIAL: 202305 & 202307 -- data shift -16
 
+    return data
+
+
+def hf_sid2_add(data, data1):
+    """
+    input:  data, data1
+    return: data
+    """
+
+    # AUX
+    data.U_selected = np.r_["0", data.U_selected, data1.U_selected]
+    data.V_selected = np.r_["0", data.V_selected, data1.V_selected]
+    data.W_selected = np.r_["0", data.W_selected, data1.W_selected]
+    data.cal_signal = np.r_["0", data.cal_signal, data1.cal_signal]
+    data.sweep_table = np.r_["0", data.sweep_table, data1.sweep_table]
+    #
+    data.onboard_cal = np.r_["0", data.onboard_cal, data1.onboard_cal]
+    data.complex = np.r_["0", data.complex, data1.complex]
+    data.BG_subtract = np.r_["0", data.BG_subtract, data1.BG_subtract]
+    data.BG_select = np.r_["0", data.BG_select, data1.BG_select]
+    data.FFT_window = np.r_["0", data.FFT_window, data1.FFT_window]
+    data.RFI_rejection = np.r_["0", data.RFI_rejection, data1.RFI_rejection]
+    data.Pol_sep_thres = np.r_["0", data.Pol_sep_thres, data1.Pol_sep_thres]
+    data.Pol_sep_SW = np.r_["0", data.Pol_sep_SW, data1.Pol_sep_SW]
+    data.overflow_U = np.r_["0", data.overflow_U, data1.overflow_U]
+    data.overflow_V = np.r_["0", data.overflow_V, data1.overflow_V]
+    data.overflow_W = np.r_["0", data.overflow_W, data1.overflow_W]
+    data.proc_param0 = np.r_["0", data.proc_param0, data1.proc_param0]
+    data.proc_param1 = np.r_["0", data.proc_param1, data1.proc_param1]
+    data.proc_param2 = np.r_["0", data.proc_param2, data1.proc_param2]
+    data.proc_param3 = np.r_["0", data.proc_param3, data1.proc_param3]
+    data.BG_downlink = np.r_["0", data.BG_downlink, data1.BG_downlink]
+    data.N_block = np.r_["0", data.N_block, data1.N_block]
+    data.T_RWI_CH1 = np.r_["0", data.T_RWI_CH1, data1.T_RWI_CH1]
+    data.T_RWI_CH2 = np.r_["0", data.T_RWI_CH2, data1.T_RWI_CH2]
+    data.T_HF_FPGA = np.r_["0", data.T_HF_FPGA, data1.T_HF_FPGA]
+
+    # Header
+    data.N_samp = np.r_["0", data.N_samp, data1.N_samp]
+    data.N_step = np.r_["0", data.N_step, data1.N_step]
+    data.decimation = np.r_["0", data.decimation, data1.decimation]
+    data.pol = np.r_["0", data.pol, data1.pol]
+    data.B0_startf = np.r_["0", data.B0_startf, data1.B0_startf]
+    data.B0_stopf = np.r_["0", data.B0_stopf, data1.B0_stopf]
+    data.B0_step = np.r_["0", data.B0_step, data1.B0_step]
+    data.B0_repeat = np.r_["0", data.B0_repeat, data1.B0_repeat]
+    data.B0_subdiv = np.r_["0", data.B0_subdiv, data1.B0_subdiv]
+    data.B1_startf = np.r_["0", data.B1_startf, data1.B1_startf]
+    data.B1_stopf = np.r_["0", data.B1_stopf, data1.B1_stopf]
+    data.B1_step = np.r_["0", data.B1_step, data1.B1_step]
+    data.B1_repeat = np.r_["0", data.B1_repeat, data1.B1_repeat]
+    data.B1_subdiv = np.r_["0", data.B1_subdiv, data1.B1_subdiv]
+    data.B2_startf = np.r_["0", data.B2_startf, data1.B2_startf]
+    data.B2_stopf = np.r_["0", data.B2_stopf, data1.B2_stopf]
+    data.B2_step = np.r_["0", data.B2_step, data1.B2_step]
+    data.B2_repeat = np.r_["0", data.B2_repeat, data1.B2_repeat]
+    data.B2_subdiv = np.r_["0", data.B2_subdiv, data1.B2_subdiv]
+    data.B3_startf = np.r_["0", data.B3_startf, data1.B3_startf]
+    data.B3_stopf = np.r_["0", data.B3_stopf, data1.B3_stopf]
+    data.B3_step = np.r_["0", data.B3_step, data1.B3_step]
+    data.B3_repeat = np.r_["0", data.B3_repeat, data1.B3_repeat]
+    data.B3_subdiv = np.r_["0", data.B3_subdiv, data1.B3_subdiv]
+    data.B4_startf = np.r_["0", data.B4_startf, data1.B4_startf]
+    data.B4_stopf = np.r_["0", data.B4_stopf, data1.B4_stopf]
+    data.B4_step = np.r_["0", data.B4_step, data1.B4_step]
+    data.B4_repeat = np.r_["0", data.B4_repeat, data1.B4_repeat]
+    data.B4_subdiv = np.r_["0", data.B4_subdiv, data1.B4_subdiv]
+
+    # Data
+    data.epoch = np.r_["0", data.epoch, data1.epoch]
+    data.scet = np.r_["0", data.scet, data1.scet]
+    #
+    data.Eu_i = np.r_["0", data.Eu_i, data1.Eu_i]
+    data.Eu_q = np.r_["0", data.Eu_q, data1.Eu_q]
+    data.Ev_i = np.r_["0", data.Ev_i, data1.Ev_i]
+    data.Ev_q = np.r_["0", data.Ev_q, data1.Ev_q]
+    data.Ew_i = np.r_["0", data.Ew_i, data1.Ew_i]
+    data.Ew_q = np.r_["0", data.Ew_q, data1.Ew_q]
+    data.pps_count = np.r_["0", data.pps_count, data1.pps_count] 
+    data.sweep_start = np.r_["0", data.sweep_start, data1.sweep_start]
+    data.reduction = np.r_["0", data.reduction, data1.reduction]
+    data.overflow = np.r_["0", data.overflow, data1.overflow]
+    #
+    data.time = np.r_["0", data.time, data1.time]
+    data.frequency = np.r_["0", data.frequency, data1.frequency]
+    data.freq_step = np.r_["0", data.freq_step, data1.freq_step]
+    data.freq_width = np.r_["0", data.freq_width, data1.freq_width]
+    
+    return data
+
+
+def hf_sid2_proc(data):
+    """
+    input:  data
+    return: data
+    """
+
     # CUT & Reshape
     n_time = data.Eu_i.shape[0]
     n_freq = data.N_step[0]
@@ -126,9 +223,9 @@ def juice_getdata_hf_sid2(cdf, cf):
         data.sweep_start = data.sweep_start[:, 0:n_num]
         data.reduction = data.reduction[:, 0:n_num]
         data.overflow = data.overflow[:, 0:n_num]
-        print("cut1 :", data.Eu_i.shape, n_time, "x", n_freq, "x", n_samp)
-    print(data.frequency[:, 0])
-    print(data.frequency[:, -1])
+        print(" cut1:", data.Eu_i.shape)  # , n_time, "x", n_freq, "x", n_samp)
+    # print(data.frequency[:, 0])
+    # print(data.frequency[:, -1])
 
     # Merge & CUT
     n_freq0 = n_freq
@@ -141,7 +238,7 @@ def juice_getdata_hf_sid2(cdf, cf):
             n_freq0 += data.N_step[i]
         n_time = n_time // i
         n_freq = n_freq * i
-        print(" cut2:", data.Eu_i.shape, n_time, "x", n_freq, "x", n_samp)
+        print(" cut2:", data.Eu_i.shape)  # , n_time, "x", n_freq, "x", n_samp)
         data.epoch = np.array(data.epoch).reshape(n_time, i)
         print(data.epoch.shape)
         print(data.epoch)
@@ -186,6 +283,7 @@ def juice_getdata_hf_sid2(cdf, cf):
     print("fixed:", data.Eu_i.shape, n_time, "x", n_freq, "x", n_samp)
 
     # ### SPECIAL: 202305 & 202307 -- data shift -16
+    date = data.epoch[0];  month = date.strftime('%Y%m')
     if month == "202305" or month == "202307":
         data.Eu_i[:, -1, n_samp//2:n_samp] = 0.;  data.Eu_q[:, -1, n_samp//2:n_samp] = 0.
         data.Ev_i[:, -1, n_samp//2:n_samp] = 0.;  data.Ev_q[:, -1, n_samp//2:n_samp] = 0.
